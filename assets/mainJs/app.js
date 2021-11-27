0de0393a14f5da92b95794e70b41a9d837de37f6
azerahmedov <ehmedovh111@gmail.com>
2021-11-28 01:34:42 +0400

    Modal duzeldildi

--- assets/mainJs/app.js
+++ assets/mainJs/app.js
@@ -2,10 +2,25 @@ let OpenBar = document.querySelector('.open-menu');
 let closeBar = document.querySelector('.close-btn');
 let listMobil = document.querySelector('.list-mobil')
 
+
 OpenBar.addEventListener('click', () => {
   listMobil.classList.add('active')
 })
 
 closeBar.addEventListener('click', () => {
     listMobil.classList.remove('active')
+})
+
+let openModal = document.querySelector('.form-modal');
+let formCLose = document.querySelector('.form-close');
+let ilanBtn = document.querySelector("#ilan-btn");
+let Body = document.querySelector('#body');
+
+ilanBtn.addEventListener('click', () => {
+  openModal.classList.add('active')
+  Body.classList.add('active')
+})
+
+formCLose.addEventListener('click', () => {
+  openModal.classList.remove('active')
 })
